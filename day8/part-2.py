f277c8b25f3b5660f845ac005f999f91a4815a5a
Callum Nightingale <callum.nightingale@capgemini.com>
2023-12-10 23:18:52 +0000

    feat: add day 8 solution

---
+++ day8/part-2.py
@@ -0,0 +1,43 @@
+import math
+
+lines = []
+input_file = open("input.txt", "r")
+for line in input_file:
+    lines.append(line.strip())
+input_file.close()
+
+directions = list(lines[0].replace('L', '0').replace('R', '1'))
+
+nodes = {}
+for line in lines[2:len(lines)]:
+    split = line.split('=')
+    node = split[0].strip()
+    split_dirs = split[1].replace('(', '').replace(')', '').split(', ')
+    left = split_dirs[0].strip()
+    right = split_dirs[1].strip()
+    nodes[node] = (left, right)
+
+start_nodes = []
+for node in nodes.keys():
+    if node[-1] == 'A':
+        start_nodes.append(node)
+
+
+def solve_steps(start):
+    current_node = start
+    dir_index = 0
+    count_steps = 0
+    while current_node[-1] != 'Z':
+        node = nodes[current_node]
+        next_node = node[int(directions[dir_index])]
+        dir_index = (dir_index + 1) % len(directions)
+        current_node = next_node
+        count_steps += 1
+    return count_steps
+
+
+solved_list = []
+for node in start_nodes:
+    solved_list.append(solve_steps(node))
+
+print(math.lcm(*solved_list))
